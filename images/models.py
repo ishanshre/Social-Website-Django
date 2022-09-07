47c3be7491cf32753ca0cc779923da6e7c0d22ea
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-09-07 09:51:44 +0545

    add post model in image app

--- images/models.py
+++ images/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+import uuid
+from datetime import datetime
+from accounts.models import Profile
 # Create your models here.
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="post_images")
+    caption = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.id
